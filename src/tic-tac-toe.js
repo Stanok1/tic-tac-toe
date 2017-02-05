779168afeab4c6d1724a59612d2e963e7ca23b57
Stanok1 <konats96@gmail.com>
2017-02-05 17:47:09 +0300

    tic-tac-toe:finished

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,5 +1,7 @@
 class TicTacToe {
     constructor() {
+
+        this.winner = null;
         this.cps = 'x';
         this.field = [
             [null, null, null],
@@ -21,29 +23,65 @@ class TicTacToe {
                 this.cps = 'x'
             }
         }
-
-
-
     }
 
     isFinished() {
+      if (this.getWinner() != null || this.isDraw() == true){
+        return true;
+      }else {
+        return false;
+      }
 
     }
 
     getWinner() {
+        for (let i = 0; i < 3; i++) {
+            if (this.field[i][0] == this.field[i][1] && this.field[i][0] == this.field[i][2]) {
+                this.winner = this.field[i][0];
+            }
 
+            if (this.field[0][i] == this.field[1][i] && this.field[0][i] == this.field[2][i]) {
+                this.winner = this.field[0][i];
+            }
+        }
+
+        if (this.field[2][0] == this.field[1][1] && this.field[2][0] == this.field[0][2]) {
+            this.winner = this.field[2][0];
+        }
+        if (this.field[0][0] == this.field[1][1] && this.field[0][0] == this.field[2][2]) {
+            this.winner = this.field[0][0];
+        }
+        return this.winner;
     }
 
-    noMoreTurns() {
-
+    noMoreTurns() { //true if game field is full
+      this.nmt = 0;
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (this.field[i][j] == null) {
+                    this.nmt++;
+                }
+            }
+        }
+              if (this.nmt == 0){
+                  return true;
+                }else {
+                  return false;
+                }
     }
 
     isDraw() {
+        if (this.noMoreTurns() == false || this.getWinner() != null) {
+
+            return false;
+        } else {
+            return true;
+        }
 
     }
 
     getFieldValue(rowIndex, colIndex) {
-    return this.field[rowIndex][colIndex];
+        return this.field[rowIndex][colIndex];
     }
 }
 
