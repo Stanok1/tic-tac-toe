1e5b7423003fc4956481f046a70532985e34249b
Stanok1 <konats96@gmail.com>
2017-02-05 13:35:13 +0300

    init

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,13 +1,28 @@
 class TicTacToe {
     constructor() {
-
+        this.cps = 'x';
+        this.field = [
+            [null, null, null],
+            [null, null, null],
+            [null, null, null]
+        ];
     }
 
     getCurrentPlayerSymbol() {
-
+        return this.cps;
     }
 
     nextTurn(rowIndex, columnIndex) {
+        if (this.field[rowIndex][columnIndex] == null) {
+            this.field[rowIndex][columnIndex] = this.cps;
+            if (this.cps == 'x') {
+                this.cps = 'o';
+            } else {
+                this.cps = 'x'
+            }
+        }
+
+
 
     }
 
@@ -28,7 +43,7 @@ class TicTacToe {
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+    return this.field[rowIndex][colIndex];
     }
 }
 
